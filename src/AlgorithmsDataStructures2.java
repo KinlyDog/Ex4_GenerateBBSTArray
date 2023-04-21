736cd39dc0cde7c16a98c5a6bf7d0c021eebfee7
KinlyDog <kinlydog@gmail.com>
2023-04-21 20:14:34 +1000

    Remake project

--- src/AlgorithmsDataStructures2.java
+++ src/AlgorithmsDataStructures2.java
@@ -2,28 +2,22 @@ import java.util.*;
 
 public class AlgorithmsDataStructures2 {
     public static int[] GenerateBBSTArray(int[] a) {
-        int[] sort = Arrays.copyOf(a, a.length);
-        Arrays.sort(sort);
+        Arrays.sort(a);
+        int[] result = new int[a.length];
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i : sort) {
-            list.add(0, i);
-        }
-
-        GenerateBBSTArrayRec(sort.length - 1, sort, list);
-
-        return sort;
+        GenerateBBSTArrayRec(a, 0, a.length - 1, result, 0);
+        return result;
     }
 
-    private static void GenerateBBSTArrayRec(int i, int[] sort, ArrayList<Integer> list) {
-        if (list.isEmpty()) {
+    private static void GenerateBBSTArrayRec(int[] sort, int start, int end, int[] result, int i) {
+        if (start > end) {
             return;
         }
 
-        for (int t = 0; t < list.size(); t++) {
-            sort[i--] = list.remove(t);
-        }
+        int mid = (start + end) / 2;
+        result[i] = sort[mid];
 
-        GenerateBBSTArrayRec(i, sort, list);
+        GenerateBBSTArrayRec(sort, start, mid - 1, result, 2 * i + 1);
+        GenerateBBSTArrayRec(sort, mid + 1, end, result, 2 * i + 2);
     }
 }
